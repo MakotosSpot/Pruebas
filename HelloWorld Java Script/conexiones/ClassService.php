1ddac8edbbdaec59e891b2ef12a9e84f2f29a6cf
Makoto's Spot <91150465+MakotosSpot@users.noreply.github.com>
2021-09-21 16:15:44 -0600

    Update ClassService.php

--- HelloWorld Java Script/conexiones/ClassService.php
+++ HelloWorld Java Script/conexiones/ClassService.php
@@ -1,7 +1,7 @@
 <?php
 		function WebServJSon($ruta, $JSon)
 			{
-				$Serv="192.168.113.150";
+				$Serv="IPServ";
 				$wsdl = "http://".$Serv."/WebRestService.asmx?WSDL";
 				$soapClient=new nusoap_client($wsdl, true); 				
 				$resultado = $soapClient->call($ruta,array("JSon"=>$JSon));
